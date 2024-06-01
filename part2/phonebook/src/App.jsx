f393abf38094431905bc5b59d23b3c8812cb44c9
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-06-01 20:42:17 -0300

    feat: add exercise solution for 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const InputText = ({ label, value, setValue }) => {
   return (
@@ -71,14 +72,15 @@ const PhonebookList = ({ phonebook, searchFilter }) => {
 const App = () => {
   // TODO: use regex for searching...
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [searchFilter, setSearchFilter] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   return (
     <div>
